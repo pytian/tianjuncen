a58edf9f9d013aec8bcea1abf4d98028e9ff103a
tianjuncen <tianjuncen@haxitag.com>
2019-09-24 14:32:00 +0800

    test

---
+++ static/js/main.js
@@ -0,0 +1,65 @@
+$(function () {
+    if ($("div").children(".title").length >= 300) {
+        var len = 300;
+    } else {
+        var len = $("div").children(".title").length;
+    }
+    var pages = Math.ceil(len / 30);
+    pagination(1);
+    var html = "<ul class=\"pagination\">\n" +
+        "    <li onclick='pagination(0)'><button>上一页</button></li>    ";
+    for (var i = 1; i <= pages; i++) {
+        html = html + "<li onclick='pagination(" + i + ")'><button>" + i + "</button></li>"
+    }
+
+
+    html = html + "<li onclick='pagination(999)'><button>下一页</button></li>\n" +
+        "</ul>";
+
+
+    $("#pagination").html(html);
+
+    console.log($('.nav_item2').length);
+    //获取li的所有宽度
+    var width = 0;
+    var width2 = 0;
+    for (let i = 0; i < $('.nav_item').length; i++) {
+        width += $('.nav_item').eq(i).outerWidth(true);
+    }
+    $('.nav_mine').width(width + 30);  //width只是内容的宽度，需要加上padding的宽度
+
+    for (let j = 0; j < $('.nav_item2').length; j++) {
+        width2 += $('.nav_item2').eq(j).outerWidth(true);
+    }
+    $('.nav_mine2').width(width2 + 30);  //width只是内容的宽度，需要加上padding的宽度
+
+
+});
+
+function pagination(page) {
+    var title_num = 30;
+    if (page == 0) {
+        var current_page = parseInt($("#current-page").val()) - 1;
+        $("#current-page").val(current_page);
+        $(".title:visible").hide();
+        for (var i = (current_page - 1) * title_num; i <= current_page * title_num; i++) {
+            $(".title:eq(" + i + ")").show();
+        }
+
+    } else if (page == 999) {
+        var current_page1 = parseInt($("#current-page").val()) + 1;
+        $("#current-page").val(current_page1);
+        $(".title:visible").hide();
+        for (var z = (current_page1 - 1) * title_num; z <= current_page1 * title_num; z++) {
+            $(".title:eq(" + z + ")").show();
+        }
+
+    } else {
+        $(".title:visible").hide();
+        for (var j = (page - 1) * title_num; j <= page * title_num; j++) {
+            $(".title:eq(" + j + ")").show();
+        }
+        $("#current-page").val(page)
+    }
+
+}
